393d2df5f1c805be31ab0016ce5d5e3ea3f6b221
raBeta <raul.bethencourt.pro@gmail.com>
2022-03-15 21:29:52 +0100

    :construction: finish ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
+    // Memory and Allocation
     fn first() {
         let x: &str = "hello ";
         second(x);
@@ -25,5 +26,49 @@ fn main() {
     }
 
     first();
-    // Memory and Allocation
+
+    // String Slices
+    let s = String::from("One hello world");
+
+    let word = first_word(&s);
+
+    println!("{}", word);
+
+    let my_string = String::from("hello world");
+
+    // `first_word` works on slices of `String`s, whether partial or whole
+    let word = first_word(&my_string[0..6]);
+    println!("{}", word);
+    let word = first_word(&my_string[..]);
+    println!("{}", word);
+    // `first_word` also works on references to `String`s, which are equivalent
+    // to whole slices of `String`s
+    let word = first_word(&my_string);
+    println!("{}", word);
+
+    let my_string_literal = "hello world";
+
+    // `first_word` works on slices of string literals, whether partial or whole
+    let word = first_word(&my_string_literal[0..6]);
+    println!("{}", word);
+    let word = first_word(&my_string_literal[..]);
+    println!("{}", word);
+
+    // Because string literals *are* string slices already,
+    // this works too, without the slice syntax!
+    let word = first_word(my_string_literal);
+    println!("{}", word);
+}
+
+// Return string slice
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+
+    &s[..]
 }
