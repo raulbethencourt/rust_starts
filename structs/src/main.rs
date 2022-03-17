6e2a145b39cedaf184b5c0c161c4d5af93a46b26
raBeta <raul.bethencourt.pro@gmail.com>
2022-03-17 23:10:15 +0100

    :construction: structs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -4,6 +4,8 @@ struct User {
     email: String,
     sign_in_count: u64,
 }
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
 
 fn main() {
     let mut user1 = User {
@@ -16,10 +18,20 @@ fn main() {
     user1.username = String::from("raBeta");
     let user2 = build_user(String::from("my@test.mail"), String::from("someuser123"));
 
-    println!("{} {}", user1.username, user2.email);
+    // struct update syntax
+    let user3 = User {
+        username: String::from("difool"),
+        ..user1
+    };
+
+    println!("{} {} {}", user1.username, user2.email, user3.email);
+
+    let _black = Color(0, 0, 0);
+    let _origin = Point(0, 0, 0);
 }
 
 fn build_user(email: String, username: String) -> User {
+    // field init shorthand
     User {
         email,
         username,
