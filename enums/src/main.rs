ffaab6d7882354a7777e14b03041c480ac0b0e5f
raBeta <raul.bethencourt.pro@gmail.com>
2022-03-20 21:44:29 +0100

    :construction: enums

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,3 +1,12 @@
+#[derive(Debug)]
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
 fn main() {
-    println!("Hello, world!");
+    let home = IpAddr::V4(127, 0, 0, 1);
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    dbg!(home, loopback);
 }
