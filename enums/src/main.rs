081d5424eac12fcc5cc9bb63caab55bde0498196
raBeta <raul.bethencourt.pro@gmail.com>
2022-04-10 21:25:53 +0200

    test somme fn

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -4,9 +4,33 @@ enum IpAddr {
     V6(String),
 }
 
+#[derive(Debug)]
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        dbg!(&self);
+    }
+}
+
 fn main() {
     let home = IpAddr::V4(127, 0, 0, 1);
     let loopback = IpAddr::V6(String::from("::1"));
 
     dbg!(home, loopback);
+
+    let help = Message::Write(String::from("Socorroooo !!"));
+    help.call();
+
+    let some_number = Some(5);
+    let some_string = Some("a string");
+
+    let absent_number: Option<i32> = None;
+
+    dbg!(some_number, some_string, absent_number);
 }
