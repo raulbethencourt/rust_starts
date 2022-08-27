8b8811c7e8a500406be93bf8dad3774b07cf06d1
raBeta <raul.bethencourt.pro@gmail.com>
2022-08-27 21:13:50 +0200

    :construction: start error handling

--- collections/src/hashmap.rs
+++ collections/src/hashmap.rs
@@ -1,10 +1,9 @@
 use std::collections::HashMap;
 
-/// Creates a new hash map with the default size.
+/// Creates a new hash mp with the default size.
 pub fn set_hashmap(color: String, number: i32) -> HashMap<String, i32> {
     let mut scores: HashMap<String, i32> = HashMap::new();
 
     scores.insert(color, number);
-
     scores
 }
