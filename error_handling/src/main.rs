febfecc7b7e24146c69e48c0682d7bf936f306a6
raBeta <raul.bethencourt.pro@gmail.com>
2022-10-22 23:50:59 +0200

    :construction: testing

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -2,10 +2,10 @@ use std::{fs::File, io::ErrorKind};
 
 fn main() {}
 
-fn exemples() {
+fn examples() {
     let greeting_file_result = File::open("hello.txt");
 
-    let _greeting_file = match greeting_file_result {
+    let greeting_file = match greeting_file_result {
         Ok(file) => file,
         Err(error) => match error.kind() {
             ErrorKind::NotFound => match File::create("hello.txt") {
