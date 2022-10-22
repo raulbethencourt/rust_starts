7c09528a7767f425f1081f04094f197ff1c461e9
raBeta <raul.bethencourt.pro@gmail.com>
2022-10-22 14:47:26 +0200

    [WORK] continue errors

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,9 +1,10 @@
 use std::{fs::File, io::ErrorKind};
 
-fn main() {
+fn main() {}
+
+fn exemples() {
     let greeting_file_result = File::open("hello.txt");
 
-    // TODO!continue with alternatives
     let _greeting_file = match greeting_file_result {
         Ok(file) => file,
         Err(error) => match error.kind() {
@@ -16,4 +17,19 @@ fn main() {
             }
         },
     };
+
+    let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+
+    let greeting_file =
+        File::open("hello.txt").expect("hello.txt should be included in this project");
+    
+        //TODO!continue propagating errors
 }
