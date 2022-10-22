89a3ca94d6bde4646b12a01cb5e76da2298e7d35
raBeta <raul.bethencourt.pro@gmail.com>
2022-10-22 14:04:20 +0200

    :construction: error

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -3,15 +3,15 @@ use std::{fs::File, io::ErrorKind};
 fn main() {
     let greeting_file_result = File::open("hello.txt");
 
-    /// TODO!continue with alternatives
-    let greeting_file = match greeting_file_result {
+    // TODO!continue with alternatives
+    let _greeting_file = match greeting_file_result {
         Ok(file) => file,
         Err(error) => match error.kind() {
             ErrorKind::NotFound => match File::create("hello.txt") {
                 Ok(fc) => fc,
                 Err(e) => panic!("Problem creating the file: {:?}", e),
             },
-            other_error => {
+            _other_error => {
                 panic!("Problem opening the file: {:?}", error);
             }
         },
