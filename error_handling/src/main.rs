4fc512a29acedcd8bd53b2cc016a8f0d275fc7a2
raBeta <raul.bethencourt.pro@gmail.com>
2022-10-23 15:35:35 +0200

    [WORK] working with errors

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,4 +1,8 @@
-use std::{fs::File, io::ErrorKind};
+use std::{
+    fs::File,
+    io::ErrorKind,
+    io::{self, Read},
+};
 
 fn main() {}
 
@@ -30,6 +34,22 @@ fn examples() {
 
     let greeting_file =
         File::open("hello.txt").expect("hello.txt should be included in this project");
-    
-        //TODO!continue propagating errors
+
+    //TODO!continue propagating errors
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("hello.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
 }
