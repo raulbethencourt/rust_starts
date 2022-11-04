d40a693961ecec53eaaaed12eb1a15b3ebf5527d
raBeta <raul.bethencourt.pro@gmail.com>
2022-11-04 22:53:50 +0100

    :construction: finish with error handling

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,15 +1,17 @@
 mod save;
 use save::examples;
+use std::net::IpAddr;
 
-use std::{
-    error::Error,
-    fs::{self, File},
-    io,
-};
+use std::{error::Error, fs::File, io};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let f: Result<File, io::Error> = File::open("hello.txt");
     examples();
+
+    // use expect coz we have more information than compiler
+    let home: IpAddr = "127.0.0.1"
+        .parse()
+        .expect("Hardcoded IP address should be valid");
 
     Ok(())
 }
--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,14 +1,18 @@
 use colored::*;
 use rand::Rng;
-use std::cmp::Ordering;
-use std::io;
+use std::{cmp::Ordering, io};
 
+/// .
+///
+/// # Panics
+///
+/// Panics if .
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1, 100);
+    let secret_number: i32 = rand::thread_rng().gen_range(1, 100);
 
-    loop {
+    'l: loop {
         println!("Please input your guess.");
 
         let mut guess = String::new();
@@ -17,17 +21,45 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = match guess.trim().parse() {
+        let value: i32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue,
+            Err(_) => continue 'l,
         };
+
+        // use Guess struct
+        let guess: i32 = Guess::new(value).value();
 
         println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("{}", "Too small!!".red()),
             Ordering::Greater => println!("{}", "Too big!!".red()),
-            Ordering::Equal => break println!("{}", "You win!!".green()),
+            Ordering::Equal => break 'l println!("{}", "You win!!".green()),
         }
+    }
+}
+
+#[derive()]
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    /// Creates a new [`Guess`].
+    ///
+    /// # Panics
+    ///
+    /// Panics if .
+    pub fn new(value: i32) -> Guess {
+        if !(1..=100).contains(&value) {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
+
+    /// Returns the value of this [`Guess`].
+    pub fn value(&self) -> i32 {
+        self.value
     }
 }
