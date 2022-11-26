29314a34d205cecc68a05973fe4167b2fa53788e
raBeta <raul.bethencourt.pro@gmail.com>
2022-11-26 22:58:59 +0100

    :construction: removing duplication

--- traits_lifetimes/src/main.rs
+++ traits_lifetimes/src/main.rs
@@ -1,13 +1,23 @@
-fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
+fn largest(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
 
-    let mut largest = &number_list[0];
-
-    for number in &number_list {
-        if number > largest {
-            largest = number;
+    for item in list {
+        if item > largest {
+            largest = item;
         }
     }
 
-    println!("The largest number is {}", largest);
+    largest
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
 }
